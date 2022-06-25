8ae7c63ddfc451c0737a2f68aad584b80dd603cf
NU-Faysol <nizamfaysol16@gmail.com>
2022-06-25 22:36:08 +0600

    first commit

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="utils.css">
+
+</head>
+<body class='overflow-x-hidden'>
+    <div class="container mx-auto">
+
+    <header>
+            <nav class="flex justify-between">
+                <div class="logo font-bold flex items-center text-blue">Faysol Fitness</div>
+                <ul class="navbar flex items-center">
+                    <li><a class="anchor" href="index.html"> Home </a></li>
+                    <li><a class="anchor" href="about.html"> About </a></li>
+                    <li><a class="anchor" href="services.html"> Services </a></li>
+                    <li><a class="anchor" href="contractUs.html"> Contact Us </a></li>
+                    <li><a class="anchor" href="registration.php"> Registration </a></li>
+                    <li><a class="anchor btn" href="login.php">Login</a></li>
+                </ul>
+            </nav>
+            <hr>
+        </header>
+    </div>
+
+</body>
+</html>
