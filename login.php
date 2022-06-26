bcdca4866fc8aba94b79540ffc8cd3bb4515b020
NU-Faysol <nizamfaysol16@gmail.com>
2022-06-27 04:05:12 +0600

    todays task is done

--- login.php
+++ login.php
@@ -10,23 +10,30 @@
 
 </head>
 <body class='overflow-x-hidden'>
-    <div class="container mx-auto">
+    <div class="Container mx-auto">
 
     <header>
-            <nav class="flex justify-between">
-                <div class="logo font-bold flex items-center text-blue">Faysol Fitness</div>
-                <ul class="navbar flex items-center">
+            <nav class="Flex justify-between">
+                <div class="logo font-bold Flex items-center text-blue">Faysol Fitness</div>
+                <ul class="navbar Flex items-center">
                     <li><a class="anchor" href="index.html"> Home </a></li>
                     <li><a class="anchor" href="about.html"> About </a></li>
                     <li><a class="anchor" href="services.html"> Services </a></li>
                     <li><a class="anchor" href="contractUs.html"> Contact Us </a></li>
                     <li><a class="anchor" href="registration.php"> Registration </a></li>
-                    <li><a class="anchor btn" href="login.php">Login</a></li>
+                    <li><a class="anchor Btn" href="login.php">Login</a></li>
                 </ul>
             </nav>
             <hr>
         </header>
     </div>
 
+
+
+    <footer>
+        <h1 class="line-height-1"> <span class="text-blue">Faysol</span> Fitness</h1>
+        <p style="line-height: 30px;">Copyright &copy; <span class="text-orange">FaysolFitness.com</span></p> 
+         <p>All rights reserved</p>
+    </footer>
 </body>
 </html>
--- registration.php
+++ registration.php
@@ -8,26 +8,103 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="utils.css">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+
+.gradient-custom-4 {
+
+background: #84fab0;
+background: -webkit-linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1));
+background: linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))
+
+}
+    </style>
+
 </head>
 <body class='overflow-x-hidden'>
-    <div class="container mx-auto">
+    <div class="Container mx-auto">
 
         <header>
-            <nav class="flex justify-between">
-                <div class="logo font-bold flex items-center text-blue">Faysol Fitness</div>
-                <ul class="navbar flex items-center">
+            <nav class="Flex justify-between">
+                <div class="logo font-bold Flex items-center text-blue">Faysol Fitness</div>
+                <ul class="navbar Flex items-center">
                     <li><a class="anchor" href="index.html"> Home </a></li>
                     <li><a class="anchor" href="about.html"> About </a></li>
                     <li><a class="anchor" href="services.html"> Services </a></li>
                     <li><a class="anchor" href="contractUs.html"> Contact Us </a></li>
                     <li><a class="anchor" href="registration.php"> Registration </a></li>
-                    <li><a class="anchor btn" href="login.php">Login</a></li>
+                    <li><a class="anchor Btn" href="login.php">Login</a></li>
                 </ul>
             </nav>
             <hr>
         </header>
 
     </div>
+
+    <main>
+
+    <section class="vh-100 bg-image">
+  <div class="mask d-flex align-items-center h-100">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div class="card" style="border-radius: 15px;">
+            <div class="card-body p-5">
+              <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+
+              <form>
+
+                <div class="form-outline mb-4">
+                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example1cg">Your Name</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example3cg">Your Email</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example4cg">Password</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                </div>
+
+                <div class="form-check d-flex justify-content-center mb-5">
+                  <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
+                  <label class="form-check-label" for="form2Example3g">
+                    I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
+                  </label>
+                </div>
+
+                <div class="d-flex justify-content-center">
+                  <button type="button"
+                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                </div>
+
+                <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!"
+                    class="fw-bold text-body"><u>Login here</u></a></p>
+
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</section>
+
+
+    </main>
+
+
 
 </body>
 </html>
